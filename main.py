e6714f295d0fe6391927559d16430c27e2f36b65
Rebekah <macrosystem@proton.me>
2025-04-04 23:50:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+# libs
 
+# source view-source:https://github.com/GTFOBins/GTFOBins.github.io/tree/master/_gtfobins
+# line 1563
+#   <script type="application/json" data-target="react-app.embeddedData">{"payload":{"allShortcutsEnabled":true,"path":"_gtfobins","repo":{"id":134314037,"defaultBranch":"master","name":"GTFOBins.github.io","ownerLogin":"GTFOBins","currentUserCanPush":false,"isFork":false,"isEmpty":false,"createdAt":"2018-05-21T21:14:11.000+02:00","ownerAvatar":"https://avatars.githubusercontent.com/u/39499534?v=4","public":true,"private":false,"isOrgOwned":true},"currentUser":{"id":79146603,"login":"helldotrs","userEmail":"macrosystem@proton.me"},"refInfo":{"name":"master","listCacheKey":"v0:1730141613.0","canEdit":true,"refType":"branch","currentOid":"ac8e667a7ec1d6b109aca48da26f300c5ecd5761"},"tree":{"items":[{"name":".dir-locals.el","path":"_gtfobins/.dir-locals.el","contentType":"file"},
+# item "name":"$tool.md","path":"_gtfobins/$tool.md","contentType":"file"}
